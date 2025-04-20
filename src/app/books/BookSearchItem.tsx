b78fe382186f03ffa64805b79cf1599499fa1fa0
DiegoRojo99 <diego99rojo@gmail.com>
2025-04-20 13:01:31 +0200

    Updated book info and style

--- src/app/books/BookSearchItem.tsx
+++ src/app/books/BookSearchItem.tsx
@@ -1,14 +1,34 @@
+import Image from "next/image";
 import { Book } from "../utils/types/books";
 
 export default function BookSearchItem({ book }: { book: Book }) {
+  if (!book.volumeInfo) {
+    return <p>No book information available.</p>;
+  }
+  let bookInfo = book.volumeInfo;
+  let bookDescription = bookInfo.description || "No description available.";
+  if (bookDescription?.length > 100) { bookDescription = `${bookInfo.description.substring(0, 100)}...`; }
+  let bookTitle = bookInfo.title;
+  if(!bookTitle) return;
+  if (bookTitle?.length > 60) { bookTitle = `${bookTitle.substring(0, 60)}...`; }
+
   return (
-    <div className='flex flex-col items-center justify-center'>
-      <img
-        src={book.volumeInfo.imageLinks.thumbnail}
-        alt={book.volumeInfo.title}
-        style={{ width: '50px', height: '75px' }}
-      />
-      <p>{book.volumeInfo.title}</p>
-    </div>
+    <>
+      {bookInfo.imageLinks?.thumbnail && (
+        <Image
+          src={bookInfo.imageLinks?.thumbnail}
+          alt={bookTitle}
+          width={64}
+          height={96}
+          className="object-cover w-16 h-24 rounded-md my-auto"
+          loading="lazy"
+        />
+      )}
+      <div className='ml-2'>
+        <h3 className="font-bold">{bookTitle}</h3>
+        <p className="text-xs">{bookDescription}</p>
+        <p className="text-gray-500">{bookInfo.authors?.join(", ")}</p>
+      </div>
+    </>
   );
 }
