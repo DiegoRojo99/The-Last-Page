6cc930c228b2821b376d6a4d59155b389d091636
DiegoRojo99 <diego99rojo@gmail.com>
2025-04-20 13:01:05 +0200

    Styled results into a column of items

--- src/app/books/BookSearchResult.tsx
+++ src/app/books/BookSearchResult.tsx
@@ -1,13 +1,14 @@
 import { Book } from "../utils/types/books";
 import BookSearchItem from "./BookSearchItem";
+import './Books.css';
 
 export default function BookSearchResult({ books }: { books: Book[] }) {
   return (
     <div className='flex flex-col items-center justify-center'>
       {books.length > 0 ? (
-        <ul className='flex flex-row items-center justify-center'>
+        <ul className='flex flex-col items-center justify-center'>
           {books.map((book) => (
-            <li key={book.id} className='m-2'>
+            <li key={book.id} className='m-2 book-search-item'>
               <BookSearchItem book={book} />
             </li>
           ))}
