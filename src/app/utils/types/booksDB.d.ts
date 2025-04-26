3f877fc63e6ab1b198d8f2278167eb43ec183fe1
DiegoRojo99 <diego99rojo@gmail.com>
2025-04-26 14:04:24 +0200

    DB types

---
+++ src/app/utils/types/booksDB.d.ts
@@ -0,0 +1,54 @@
+type userBooks = {
+  id: string;
+  title: string;
+  authors: string[];
+  coverImage: string;
+  status: 'currentlyReading' | 'completed' | 'dropped';
+  startedAt?: Timestamp;
+  completedAt?: Timestamp;
+  totalPages?: number;
+  notes?: string;
+}
+
+type readingSession = {
+  id: string;
+  bookId: string;
+  durationMinutes: number;
+  pagesRead?: number;
+  sessionDate: Timestamp;
+  notes?: string;
+}
+
+type achievement = {
+  id: string;
+  title: string;
+  description: string;
+  achievedAt: Timestamp;
+  type: 'booksRead' | 'seriesCompleted' | 'authorCompleted' | 'custom';
+  relatedData?: any; // e.g. seriesName or authorName
+}
+
+type bookDB = {
+  id: string;
+  title: string;
+  authors: string[];
+  coverImage: string;
+}
+
+type libraryBook = bookDB & {
+  purchasedAt?: Timestamp;
+  format?: 'physical' | 'ebook' | 'audiobook';
+}
+
+type wishlistBook =  bookDB & {
+  addedAt: Timestamp;
+}
+
+export {
+  userBooks,
+  readingSession,
+  achievement,
+  bookDB,
+  libraryBook,
+  wishlistBook
+}
