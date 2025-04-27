cad674ec516162f011ea86728f8955f85e6eddf4
DiegoRojo99 <diego99rojo@gmail.com>
2025-04-27 16:16:47 +0200

    Page details now have full data

--- src/app/books/[id]/BookCoverBackground.tsx
+++ src/app/books/[id]/BookCoverBackground.tsx
@@ -1,14 +1,14 @@
-import { bookDB } from "@/app/utils/types/booksDB";
+import { CompleteUserBook } from "@/app/utils/types/booksAPI";
 import Image from "next/image";
 
-export default function BookCoverBackground({ book }: { book: bookDB | null }) {
+export default function BookCoverBackground({ book }: { book: CompleteUserBook | null }) {
   if (!book) {
     return <div className="h-[300px] w-full bg-gray-200 animate-pulse"></div>;
   }
   return (
     <div className="relative h-[300px] w-full">
       <Image
-        src={book?.coverImage || "/default-cover.jpg"}
+        src={book?.userInfo.coverImage || "/default-cover.jpg"}
         alt="Cover background"
         className="h-full w-full object-cover filter blur-md brightness-75"
         width={128}
@@ -16,8 +16,8 @@ export default function BookCoverBackground({ book }: { book: bookDB | null }) {
       />
       <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <Image
-          src={book?.coverImage || "/default-cover.jpg"}
-          alt={book?.title}
+          src={book?.userInfo.coverImage || "/default-cover.jpg"}
+          alt={book?.userInfo.title || "Book Cover"}
           className="w-32 h-48 object-cover rounded shadow-lg"
           width={128}
           height={193}
--- src/app/books/[id]/PageDetails.tsx
+++ src/app/books/[id]/PageDetails.tsx
@@ -1,12 +1,10 @@
 'use client';
 
-import { bookDB } from "@/app/utils/types/booksDB";
 import { useCurrentUser } from "@/lib/currentUser";
-import { db } from "@/lib/firebase";
-import { doc, getDoc } from "firebase/firestore";
 import { notFound } from "next/navigation";
 import { useEffect, useState } from "react";
 import BookCoverBackground from "./BookCoverBackground";
+import { CompleteUserBook } from "@/app/utils/types/booksAPI";
 
 interface BookPageProps {
   bookId: string;
@@ -14,7 +12,7 @@ interface BookPageProps {
 
 export default function BookDetails({ bookId }: BookPageProps) {
   const { user, loading } = useCurrentUser();
-  const [book, setBook] = useState<bookDB | null>(null);
+  const [book, setBook] = useState<CompleteUserBook | null>(null);
 
   useEffect(() => {
     if (loading) return; 
@@ -23,15 +21,28 @@ export default function BookDetails({ bookId }: BookPageProps) {
       console.log("User not logged in");
     } 
     else {
-      const bookRef = doc(db, `users/${user.uid}/books`, bookId);
       const fetchBook = async () => {
-        const bookSnap = await getDoc(bookRef);
-        if (bookSnap.exists()) {
-          const bookDetails = bookSnap.data() as bookDB;
-          setBook(bookDetails);
+        try {
+          const response = await fetch(`/api/user/books/${bookId}`, {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${await user?.getIdToken()}`,
+              "Content-Type": "application/json",
+            },
+          });
+          if (response.ok) {
+            const bookDetails = await response.json();
+            setBook(bookDetails);
+          } 
+          else if (response.status === 404) {
+            notFound();
+          } 
+          else {
+            console.error("Failed to fetch book details");
+          }
         } 
-        else {
-          notFound();
+        catch (error) {
+          console.error("Error fetching book details:", error);
         }
       };
       fetchBook();
@@ -43,8 +54,8 @@ export default function BookDetails({ bookId }: BookPageProps) {
       <BookCoverBackground book={book} />
 
       <div className="p-6 text-center">
-        <h3 className="text-2xl font-bold">{book?.title}</h3>
-        <p className="text-gray-500">{book?.authors?.join(", ")}</p>
+        <h3 className="text-2xl font-bold">{book?.volumeInfo?.title}</h3>
+        <p className="text-gray-500">{book?.volumeInfo?.authors?.join(", ")}</p>
       </div>
 
       {/* Icons row */}
