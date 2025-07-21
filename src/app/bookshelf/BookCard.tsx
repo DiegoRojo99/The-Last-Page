27146c52253a2e4f20d9628d1b1687e82ec2aa58
DiegoRojo99 <diego99rojo@gmail.com>
2025-07-21 13:40:15 +0200

    Book card styling changed

--- src/app/bookshelf/BookCard.tsx
+++ src/app/bookshelf/BookCard.tsx
@@ -10,25 +10,25 @@ interface BookCardProps {
 
 export function BookCard({ book }: BookCardProps) {
   return (
-    <a aria-label="See book details" href={`/books/${book.id}`} className="border rounded-lg flex flex-col items-center w-[128px] h-[193px]">
+    <a aria-label="See book details" href={`/books/${book.id}`} className="flex flex-col items-center w-fit h-fit">
       {/* Cover Image */}
       {book.coverImage ? (
         <Image
           src={book.coverImage} 
           alt={book.title} 
-          className="object-cover w-[128px] h-[193px] rounded-lg"
+          className="border rounded-lg object-cover w-[128px] h-[193px]"
           width={128}
           height={193}
           loading="lazy"
         />
       ) : (
-        <div className="w-full h-48 flex items-center justify-center bg-gray-200 text-gray-500 text-sm">
+        <div className="w-[128px] h-48 flex items-center justify-center bg-gray-200 text-gray-500 text-sm">
           No Cover
         </div>
       )}
 
       {/* Book Info */}
-      <div className="p-1">
+      <div className="p-1 w-[128px]">
         <h3 className="text-sm font-semibold line-clamp-1">{book.title}</h3>
         {book.authors.length > 0 && (
           <p className="text-sm text-gray-500 mt-1 line-clamp-1">
